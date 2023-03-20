aa564a4e9ce7eb6b9b2f76ab52005c4c29da12b3
Philipp Grete <pgrete@hs.uni-hamburg.de>
2023-03-20 19:32:52 +0100

    Add Parthenon 2D test

--- yt/frontends/parthenon/tests/test_outputs.py
+++ yt/frontends/parthenon/tests/test_outputs.py
@@ -5,6 +5,7 @@ from yt.loaders import load
 from yt.testing import (
     assert_allclose,
     assert_equal,
+    assert_true,
     requires_file,
     units_override_check,
 )
@@ -15,26 +16,54 @@ from yt.utilities.answer_testing.framework import (
     small_patch_amr,
 )
 
-_fields_disk = ("density", "velocity_r")
+_fields_advection2d = (
+    ("parthenon", "Advected_0_0"),
+    ("parthenon", "one_minus_advected"),
+    ("parthenon", "one_minus_advected_sq"),
+    ("parthenon", "one_minus_sqrt_one_minus_advected_sq_12"),
+    ("parthenon", "one_minus_sqrt_one_minus_advected_sq_37"),
+)
 
-disk = "KeplerianDisk/disk.out1.00000.athdf"
+# Simple 2D test (advected spherical blob) with AMR from the main Parthenon test suite
+# adjusted so that x1 != x2.
+# Ran with `./example/advection/advection-example -i ../tst/regression/test_suites/output_hdf5/parthinput.advection parthenon/mesh/nx1=128 parthenon/mesh/x1min=-1.0 parthenon/mesh/x1max=1.0 Advection/vx=2`
+# on changeset e5059ad
+advection2d = "advection_2d.out0.final.phdf"
 
 
-@requires_ds(disk)
+@requires_ds(advection2d)
 def test_disk():
-    ds = data_dir_load(disk)
-    assert_equal(str(ds), "disk.out1.00000")
+    ds = data_dir_load(advection2d)
+    assert_equal(str(ds), "advection_2d.out0.final")
     dd = ds.all_data()
-    vol = (ds.domain_right_edge[0] ** 3 - ds.domain_left_edge[0] ** 3) / 3.0
-    vol *= np.cos(ds.domain_left_edge[1]) - np.cos(ds.domain_right_edge[1])
-    vol *= ds.domain_right_edge[2].v - ds.domain_left_edge[2].v
+    # test mesh dims
+    vol = np.product(ds.domain_right_edge - ds.domain_left_edge)
+    assert_equal(vol, ds.quan(2.0, "code_length**3"))
     assert_allclose(dd.quantities.total_quantity("cell_volume"), vol)
-    for field in _fields_disk:
+    # test data
+    for field in _fields_advection2d:
 
         def field_func(name):
             return dd[name]
 
         yield GenericArrayTest(ds, field_func, args=[field])
+
+    # reading data of two fields and compare against each other (data is squared in output)
+    ad = ds.all_data()
+    assert_allclose(
+        ad[("parthenon", "one_minus_advected")] ** 2.0,
+        ad[("parthenon", "one_minus_advected_sq")],
+    )
+
+    # check if the peak is in the domain center (and at the highest refinement level)
+    dist_of_max_from_center = np.linalg.norm(
+        ad.quantities.max_location(("parthenon", "Advected_0_0"))[1:] - ds.domain_center
+    )
+
+    dx_min, dx_max = ad.quantities.extrema(("index", "dx"))
+    dy_min, dy_max = ad.quantities.extrema(("index", "dy"))
+
+    assert_true(dist_of_max_from_center < np.min((dx_min, dy_min)))
 
 
 _fields_AM06 = ("temperature", "density", "velocity_magnitude", "magnetic_field_x")
